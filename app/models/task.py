c11e539ea915e01d169a57d0c12d1afffbfc9b5b
Huynh <huynhlam56@gmail.com>
2023-10-03 23:21:42 -0700

    fixed seeders

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,6 @@
 from .db import db, environment, SCHEMA
 from datetime import datetime
+from .db import add_prefix_for_prod
 
 class Task(db.Model):
   __tablename__ = 'tasks'
@@ -8,11 +9,11 @@ class Task(db.Model):
     __table_args__ = {'schema': SCHEMA}
 
   id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
   name = db.Column(db.String(255), nullable=False)
   priority = db.Column(db.Boolean, nullable=False)
   status = db.Column(db.String, nullable=False)
   category = db.Column(db.String, nullable=True)
-  deadline = db.Column(db.DateTime, nullable=False)
+  deadline = db.Column(db.Time, nullable=False)
   created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
   updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
--- app/seeds/tasks.py
+++ app/seeds/tasks.py
@@ -1,201 +1,251 @@
 from ..models import db, Task, environment, SCHEMA
-from datetime import datetime
+from datetime import datetime, time
 from sqlalchemy.sql import text
+
+current_date = datetime.now()
 
 def seed_tasks():
   tasks = [
     Task(
       name='Pick up bananas at the market',
       user_id=1,
-      priortiy=False,
+      priority=False,
       status='not started',
-      deadline='5:00 PM',
-      category='personal'
+      deadline=time(10, 0),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Get flowers for mom',
       user_id=1,
-      priortiy=True,
+      priority=True,
       status='not started',
-      deadline='5:00 PM',
-      category='personal'
+      deadline=time(14, 30),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Finish 2 leetcode problems',
       user_id=1,
-      priortiy=True,
+      priority=True,
       status='in progress',
-      deadline='6:00 PM',
-      category='school'
+      deadline=time(20, 0),
+      category='school',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Tidy up the kitchen and do dishes',
       user_id=2,
-      priortiy=True,
+      priority=True,
       status='in progress',
-      deadline='6:00 PM',
-      category='personal'
+      deadline=time(20, 0),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Start on project for work',
       user_id=2,
-      priortiy=True,
+      priority=True,
       status='in progress',
-      deadline='3:00 PM',
-      category='work'
+      deadline=time(16, 20),
+      category='work',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Do 30mins yoga session',
       user_id=2,
-      priortiy=False,
+      priority=False,
       status='not started',
-      deadline='9:00 AM',
-      category='personal'
+      deadline=time(9, 0),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Finish employee report',
       user_id=3,
-      priortiy=True,
+      priority=True,
       status='in progress',
-      deadline='3:00 PM',
-      category='work'
+      deadline=time(11, 30),
+      category='work',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Pick up dry cleaning',
       user_id=3,
-      priortiy=True,
+      priority=True,
       status='not started',
-      deadline='5:00 PM',
-      category='personal'
+      deadline=time(16, 0),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Pick up birthday gift for John',
       user_id=3,
-      priortiy=False,
+      priority=False,
       status='not started',
-      deadline='4:00 PM',
-      category='personal'
+      deadline=time(14, 0),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Finish coding assignment',
       user_id=4,
-      priortiy=True,
+      priority=True,
       status='in progress',
-      deadline='1:00 PM',
-      category='school'
+      deadline=time(14, 20),
+      category='school',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Watch lecture video on AWS',
       user_id=4,
-      priortiy=True,
+      priority=True,
       status='not started',
-      deadline='3:00 PM',
-      category='school'
+      deadline=time(17, 0),
+      category='school',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Prepare lunch for tomorrow',
       user_id=4,
-      priortiy=True,
+      priority=True,
       status='not started',
-      deadline='8:00 PM',
-      category='personal'
+      deadline=time(19, 0),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name="Email boss about tomorrow's meeting",
       user_id=5,
-      priortiy=True,
+      priority=True,
       status='not started',
-      deadline='10:00 AM',
-      category='work'
+      deadline=time(10, 0),
+      category='work',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Finish employee report',
       user_id=5,
-      priortiy=True,
+      priority=True,
       status='in progress',
-      deadline='3:00 PM',
-      category='work'
+      deadline=time(15, 0),
+      category='work',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Go to DMV',
       user_id=5,
-      priortiy=True,
+      priority=True,
       status='in progress',
-      deadline='12:00 PM',
-      category='personal'
+      deadline=time(12, 0),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Call mom',
       user_id=6,
-      priortiy=False,
+      priority=False,
       status='not started',
-      deadline='3:00 PM',
-      category='personal'
+      deadline=time(16, 0),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Check assignment board for homework',
       user_id=6,
-      priortiy=True,
+      priority=True,
       status='in progress',
-      deadline='4:00 PM',
-      category='school'
+      deadline=time(16, 0),
+      category='school',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Ask Emma to switch shifts for next week',
       user_id=6,
-      priortiy=True,
+      priority=True,
       status='not started',
-      deadline='3:00 PM',
-      category='work'
+      deadline=time(15, 0),
+      category='work',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Finish employee report',
       user_id=7,
-      priortiy=True,
+      priority=True,
       status='in progress',
-      deadline='2:00 PM',
-      category='work'
+      deadline=time(14, 40),
+      category='work',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Pick up milk',
       user_id=7,
-      priortiy=False,
+      priority=False,
       status='not started',
-      deadline='3:00 PM',
-      category='personal'
+      deadline=time(17, 20),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Take Zoe to the vet',
       user_id=7,
-      priortiy=True,
+      priority=True,
       status='not started',
-      deadline='5:00 PM',
-      category='personal'
+      deadline=time(13, 40),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Finish setting up KanBan board for work',
       user_id=8,
-      priortiy=True,
+      priority=True,
       status='in progress',
-      deadline='3:00 PM',
-      category='work'
+      deadline=time(19, 0),
+      category='work',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Return packages at UPS',
       user_id=8,
-      priortiy=True,
+      priority=True,
       status='not started',
-      deadline='1:00 PM',
-      category='personal'
+      deadline=time(17, 10),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
     ),
     Task(
       name='Pick up dinner',
       user_id=8,
-      priortiy=True,
+      priority=True,
       status='not started',
-      deadline='6:00 PM',
-      category='personal'
-    ),
+      deadline=time(18, 10),
+      category='personal',
+      created_at=current_date,
+      updated_at=current_date
+    )
   ]
   db.session.add_all(tasks)
   db.session.commit()
