62e1b6440b2c72f72011cf5ce9370d93973ccbe4
Huynh <huynhlam56@gmail.com>
2023-10-04 12:55:29 -0700

    added notes data

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from .db import db
 from .user import User
 from .db import environment, SCHEMA
 from .task import Task
+from .note import Note
---
+++ app/models/note.py
@@ -0,0 +1,14 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Note(db.Model):
+  __tablename__ = 'notes'
+
+  if environment == "production":
+    __table_args__ = {'schema': SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  task_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("tasks.id")))
+  note = db.Column(db.String(255), nullable=False)
+  created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+  updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
