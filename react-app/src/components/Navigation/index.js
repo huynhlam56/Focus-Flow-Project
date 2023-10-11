aae7db2350a082462ef7760fc49084e63baf346f
Huynh <huynhlam56@gmail.com>
2023-10-11 02:38:01 -0700

    added css

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
+	// if (sessionUser) return <Redirect to="/" />;
 
 	return (
 		<ul>
 			<li>
-				<NavLink exact to="/">Home</NavLink>
+				<NavLink exact to="/tasks">Home</NavLink>
 			</li>
 			{isLoaded && (
 				<li>
--- react-app/src/components/Task/TaskIndex/index.js
+++ react-app/src/components/Task/TaskIndex/index.js
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import './TaskIndex.css'
-import { fetchAllTasksThunk, fetchSingleTaskThunk, updateTaskThunk } from "../../../store/task";
+import { fetchAllTasksThunk, updateTaskThunk } from "../../../store/task";
 import OpenModalButton from "../../OpenModalButton";
 import SingleTask from "../SingleTask";
-import { useModal } from "../../../context/Modal";
 import CreateTask from "../CreateTask";
+import { withRouter, useHistory } from "react-router-dom";
 
 function AllTasks() {
-
   const dispatch = useDispatch()
   const tasks = useSelector(state => state.tasks.Tasks)
+  const user = useSelector(state => state.session.user)
 
   useEffect(() => {
     dispatch(fetchAllTasksThunk())
 
   },[dispatch])
 
+  // if(!user || Object.keys(user).length === 0) return (
+  //   <div>Please log in to view tasks</div>
+  // )
+  
   const handleEditSubmit = async(e, task) => {
     e.preventDefault()
    dispatch(updateTaskThunk(task, task.id))
@@ -28,7 +32,6 @@ function AllTasks() {
   const personalTasks = allTasks.filter((task) => task.category === 'Personal')
   const schoolTasks = allTasks.filter((task) => task.category === 'School')
   const workTasks = allTasks.filter((task) => task.category === 'Work')
-
 
   return (
     <div>
@@ -78,4 +81,4 @@ function AllTasks() {
   )
 }
 
-export default AllTasks;
+export default withRouter(AllTasks);
