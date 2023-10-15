581f6779f58434074a0460e9abdcdedbae6ad251
Huynh <huynhlam56@gmail.com>
2023-10-15 15:50:16 -0700

    landing-page

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -6,20 +6,45 @@ import './Navigation.css';
 import { faListCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import LoginFormModal from '../LoginFormModal';
+import logo from '../images/logo.png'
+import img from '../images/flowers.jpg'
+import OpenModalButton from '../OpenModalButton';
+import SignupFormModal from '../SignupFormModal';
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 	// if (sessionUser) return <Redirect to="/" />;
 
 	if (!sessionUser) return (
-		<ul className='header-nouser'>
-			<h1 className='title-section'>
-				<NavLink className='title' exact to="/">Focus Flow</NavLink>
-			</h1>
-			<li>
-				<ProfileButton user={sessionUser} />
-			</li>
-		</ul>
+		<div className='landing-page'>
+			<ul className='header-nouser'>
+				<h1 className='title-section'>
+					<NavLink className='title' exact to="/">Focus Flow</NavLink>
+					<img className='logo' src={logo}></img>
+				</h1>
+				<li>
+					{/* <ProfileButton user={sessionUser} /> */}
+					<OpenModalButton
+						buttonText='Start For Free'
+						modalComponent={<SignupFormModal />}
+						styleClass='Sign-up-btn'
+					/>
+					<OpenModalButton
+						buttonText='Log In'
+						modalComponent={<LoginFormModal />}
+						styleClass='Sign-up-btn'
+					/>
+				</li>
+			</ul>
+			<div>
+				<img></img>
+				<h1>From overwhelmed to on top of it</h1>
+				<h2>Focus Flow gives you the confidence that everything's
+					organized and accounted for, so you can make progress on
+					the things that are important to you
+				</h2>
+			</div>
+		</div>
 	)
 
 	return (
